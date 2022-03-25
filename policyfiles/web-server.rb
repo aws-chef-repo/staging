2ab4dda35ce66c99844b7695840d33d21f5d9fc0
r-goto <r-goto@myMac-16.local>
2022-03-25 12:15:54 +0900

    Changed to nginx v1.0.0

--- policyfiles/web-server.rb
+++ policyfiles/web-server.rb
@@ -7,7 +7,7 @@ name 'web'
 default_source :chef_server, 'https://chef-automate.creationline.com/organizations/first-org'
 run_list 'node_setup::default'#, 'nginx_setup::default'
 cookbook 'node_setup', '1.0.0'
-cookbook 'nginx_setup', '2.0.0'
+cookbook 'nginx_setup', '1.0.0'
 
 ##########
 # Attributes
