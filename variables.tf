03b42020551ed5d5b54e4c8d20c56e42d4b60b73
r-goto <r-goto@myMac-16.local>
2022-03-25 09:44:26 +0900

    Declared variables in variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,21 @@ variable "user_name" {
   type = string
   default = "ec2-user"
 }
+
+# The below is for variables' initialization. 
+# Values are all null. Specify them in respective .tfvars files. 
+
+variable "instance_count" {
+  type        = number
+  default     = 0
+}
+
+variable "policy_group" {
+  type        = string
+  default     = ""
+}
+
+variable "policy_name" {
+  type        = string
+  default     = ""
+}
